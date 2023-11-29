daf751da3a437ee590d81a7f7e6aab4adadcfd57
asandohs@gmail.com <asandohs@gmail.com>
2023-11-28 17:24:04 -0800

    task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 
 def fizzbuzz():
     for game in range(1, 101):
-        if game % 3 == 0:
+        if (game % 3 == 0) and (game % 5 == 0):
+            print("FizzBuzz", end=" ")
+        elif game % 3 == 0:
             print("Fizz", end=" ")
         elif game % 5 == 0:
             print("Buzz", end=" ")
-        elif (game % 5 == 0) and (game % 3 == 0):
-            print("FizzBuzz", end=" ")
         else:
             print(game, end=" ")
