e832f5b0e7847c4c0638c100b690d5d028c618c8
asandohs@gmail.com <asandohs@gmail.com>
2023-11-28 16:41:30 -0800

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
-# Use a for loop to iterate through the lowercase letters
-for i in range(97, 123):
-    print("{:c}".format(i), end="")
+for char in range(97, 123):
+    print("{:c}".format(char), end= "")
