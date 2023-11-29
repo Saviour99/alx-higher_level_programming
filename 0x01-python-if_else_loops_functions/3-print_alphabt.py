1c73387d5f4dcc746c8421599f2f7ab716237bcf
asandohs@gmail.com <asandohs@gmail.com>
2023-11-28 16:47:05 -0800

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
+# iterating through the chars
 for i in range(97, 123):
     if (i == 101) or (i == 113):
         continue
-    print(chr(i), end="")
+    print("{:c}".format(i), end="")
