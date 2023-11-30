4d672cb1ed719499a47e67c34645873f307d7011
asandohs@gmail.com <asandohs@gmail.com>
2023-11-30 07:55:12 -0800

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,14 +2,14 @@
 
 if __name__ == "__main__":
     # Import the add_0 module
-    import add_0
+    from add_0 import add
 
     # Assign values to a and b
     a = 1
     b = 2
 
     # Perform addition and store the result
-    result = add_0.add(a, b)
+    result = add(a, b)
 
     # Print the formatted string
     print("{:d} + {:d} = {:d}".format(a, b, result))
