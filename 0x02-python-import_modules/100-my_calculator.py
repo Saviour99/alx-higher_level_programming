20944315050ce2929913e998fee12fb9f4b5e638
asandohs@gmail.com <asandohs@gmail.com>
2023-11-30 18:48:23 -0800

    task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
 
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
-    from sys import argv as av
-    from calculator_1 import add, sub, mul, div
-
-    ac = len(av)
-    a = int(av[1])
-    b = int(av[3])
-
-    if ac != 4:
-        print("Usage: {} <a> <operator> <b>".format(av[0]))
+    if len(argv) != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
 
-    if av[2] == "+":
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    elif av[2] == "-":
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    elif av[2] == "*":
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    elif av[2] == "/":
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = sub(a, b)
+    elif operator == "*":
+        result = mul(a, b)
+    elif operator == "/":
         if b == 0:
-            print("Division by zero is not allowed")
+            print("Error: Division by zero is not allowed.")
             exit(1)
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+
+    print("{:d} {} {:d} = {:d}".format(a, operator, b, result))
     exit(0)
