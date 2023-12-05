b15d4f2de2b4ac5ab912bd6b7f525658acce2da0
asandohs@gmail.com <asandohs@gmail.com>
2023-12-05 09:53:41 -0800

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,5 @@
 def element_at(my_list, idx):
     for x in my_list:
         if x == my_list[idx]:
-            return my_list[idx]
-    else:
-        return None
+            return x
+    return None
