f9f2f90d15f18d76a2b952c39ff8a8e8c3ac5b81
asandohs@gmail.com <asandohs@gmail.com>
2023-12-06 02:02:23 -0800

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@
 def element_at(my_list, idx):
     last_index = len(my_list) - 1
     if idx < 0 or idx > last_index:
-        return None
+        return (None)
     else:
         return my_list[idx]
