858c264c6224ab0424d5cafdbc546435e3d62f2e
Saviour Assandoh <asandohs@gmail.com>
2023-12-09 10:03:33 -0800

    task 0, 1 and 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,9 @@
 #usr/bin/python3
 
 def uniq_add(my_list=[]):
-    return sum(set(my_list))
+    uniq = set(my_list)
+    result = 0
+
+    for elm in uniq:
+        result += elm
+    return result
