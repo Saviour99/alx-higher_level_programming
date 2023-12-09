d497ad4286b2f07d33cf1593b342c9663b457120
Saviour Assandoh <asandohs@gmail.com>
2023-12-09 15:29:23 -0800

    task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-        for elm in set_1:
-            if elm in set_2:
-                result = list(elm)
-            return result
+    result = [elm for elm in set_1 if elm in set_2]
+    return result
