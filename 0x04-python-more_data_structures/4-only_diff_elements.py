5f56d377b2d6d86f55154b4d468a299cdd89f04f
Saviour Assandoh <asandohs@gmail.com>
2023-12-09 16:05:30 -0800

    task 4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,11 @@
+#!sr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    result = []
+    for x in set_1:
+        if x not in set_2:
+            result.append(x)
+    for y in set_2:
+        if y not in set_1:
+            result.append(y)
+    return result
