8971e9fde7f88ec412010fc19b97e25802b20353
Saviour Assandoh <asandohs@gmail.com>
2023-12-11 00:35:33 +0000

    tasks

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,14 +1,17 @@
-#usr/bin/python3
+#!/usr/bin/python3
 
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-
-    num = 0
-    density = 0
-
-    for x in my_list:
-        num += x[0] * x[1]
-        density += x[1]
-
-    return (num / density)
+    total_score = 0
+    total_weight = 0
+    
+    for score, weight in my_list:
+        total_score += score * weight
+        total_weight += weight
+    
+    if total_weight == 0:
+        return 0
+    
+    weighted_average = total_score / total_weight
+    return weighted_average
