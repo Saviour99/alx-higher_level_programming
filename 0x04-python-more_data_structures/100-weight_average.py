ca416aa8bbc0fbbb8a6a9e49df138d08f39d01a7
Saviour Assandoh <asandohs@gmail.com>
2023-12-11 00:39:45 +0000

    tasks

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,13 +5,13 @@ def weight_average(my_list=[]):
         return 0
     total_score = 0
     total_weight = 0
-    
+
     for score, weight in my_list:
         total_score += score * weight
         total_weight += weight
-    
+
     if total_weight == 0:
         return 0
-    
+
     weighted_average = total_score / total_weight
     return weighted_average
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-	result = {x: y * 2 for x, y in a_dictionary.items()}
-	return result
+    result = {x: y * 2 for x, y in a_dictionary.items()}
+    return result
