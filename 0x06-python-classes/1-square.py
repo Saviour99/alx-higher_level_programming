65e3432fde688540a889b04b2bc61b365ec48131
Saviour Assandoh <asandohs@gmail.com>
2024-02-03 11:18:05 -0800

    task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A square module"""
+
+
+class Square:
+    """A Sqaure Class"""
+    def __init__(self, size):
+        """Initializing the object"""
+        self.__size = size
