d488783e7b9dd8bf6d0c8396726ca7a338d325ff
Saviour Assandoh <asandohs@gmail.com>
2024-02-13 19:04:14 -0800

    task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- A a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 
 USE hbtn_0c_0;
-SHOW CREATE TABLE first_table;
+SHOW CREATE TABLE `first_table`;
