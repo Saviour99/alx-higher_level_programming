34d2067f2f942601d51a9fbff7f3e06a7d01e232
Saviour Assandoh <asandohs@gmail.com>
2024-03-12 10:12:31 -0700

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
-const noArgs = "No argument";
-const oneOrMoreArgs = "Argument found";
+const noArgs = 'No argument';
+const oneOrMoreArgs = 'Argument found';
 
-if (args.length === 0){
-	console.log(noArgs);
-}
-else if (args.length === 1){
-	console.log(oneOrMoreArgs);
-}
-else {
-	console.log(oneOrMoreArgs);
+if (args.length === 0) {
+  console.log(noArgs);
+} else if (args.length === 1) {
+  console.log(oneOrMoreArgs);
+} else {
+  console.log(oneOrMoreArgs);
 }
