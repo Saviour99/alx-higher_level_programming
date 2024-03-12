008737ffc0c94402a840c8ddfb216c718ce7f44b
Saviour Assandoh <asandohs@gmail.com>
2024-03-12 10:50:48 -0700

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-const arg = process.argv[2];
-
-if (arg === undefined) {
+if (process.argv[2] === undefined) {
   console.log('No Argument');
 } else {
-  console.log(arg);
+  console.log(process.argv[2]);
 }
