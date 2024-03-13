2e3a6239fc557cad91b8abb1dc1a5d7abff12808
Saviour Assandoh <asandohs@gmail.com>
2024-03-13 05:22:37 -0700

    task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function add(a, b){
+	arg = process.argv
+	a = parseInt(process.argv[2]);
+	b = parseInt(process.argv[3]);
+	
+	if (arg.length === 4) {
+		return (a + b);
+	} else {
+		return ('NaN');
+	}
+}
+console.log(add());
