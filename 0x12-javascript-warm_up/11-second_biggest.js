85d1d058430bdf0a8eaa8b760ad3c90bf9a4763f
Saviour Assandoh <asandohs@gmail.com>
2024-03-13 07:51:52 -0700

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+if (process.argv.slice(2).length > 0) {
+  const sortArgs = process.argv.slice(2).sort((a, b) => a - b);
+  console.log(sortArgs[sortArgs.length - 2]);
+} else {
+  console.log(0);
+}
