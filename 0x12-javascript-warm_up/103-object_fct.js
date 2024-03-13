ed76b5bd2ddcada220ae6a21749e3eb36c2342f9
Saviour Assandoh <asandohs@gmail.com>
2024-03-13 10:13:14 -0700

    task 17

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,11 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-/*
-YOUR CODE HERE
-*/
+
+myObject.incr = () => {
+	myObject.value++;
+}
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
